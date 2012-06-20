7a6375a969401e8884945987fd54a5f4e6c66e1f
Tharvik <tharvik@gmail.com>
2012-06-20 12:14:22 +0200

    Initial commit

---
+++ core/CD.cpp
@@ -0,0 +1,7 @@
+#include "CD.hpp"
+
+CD::CD()
+{
+
+}
+
---
+++ core/CD.hpp
@@ -0,0 +1,18 @@
+#ifndef CD_HPP
+#define CD_HPP
+
+#include <map>
+#include <vector>
+#include <string>
+
+class CD
+{
+    public:
+        CD();
+
+    private:
+        std::map <std::string, std::vector <std::string> >
+                data;
+};
+
+#endif
